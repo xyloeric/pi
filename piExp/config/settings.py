7d2fb15ab820f0ab09a2001d34f0b8e854b0704d
Zhe Li <xyloeric@gmail.com>
2014-08-12 00:45:22 -0700

    add local app

--- piExp/config/settings.py
+++ piExp/config/settings.py
@@ -54,6 +54,7 @@ class Common(Configuration):
     LOCAL_APPS = (
         'users',  # custom users app
         # Your stuff: custom apps go here
+        'pi',
     )
 
     # See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
